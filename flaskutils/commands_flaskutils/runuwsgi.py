a0375fd3ae4bebbf776abed8f5e62bd853894ec1
Rondineli <raraujo04@yahoo.com.br>
2016-09-30 15:39:24 +0100

    Omitted debug mode on uwsgi module

--- flaskutils/commands_flaskutils/runuwsgi.py
+++ flaskutils/commands_flaskutils/runuwsgi.py
@@ -1,11 +1,8 @@
-from flaskutils.exceptions import ConfigurationError
-
 
 def run(**kwargs):
     app = kwargs['app']
     app.run(
         host=app.config['HOST'],
         port=app.config['PORT'],
-        use_reloader=False,
-        debug=app.config["DEBUG"]
+        use_reloader=False
     )
