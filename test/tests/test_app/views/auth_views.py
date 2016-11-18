a5f954ca2eff4b35cb3d19d609744bfd6e30c970
Niall Ryan <ryann4@tcd.ie>
2016-11-18 10:14:46 +0000

    removes unused flask-login basic auth loader

--- test/tests/test_app/views/auth_views.py
+++ test/tests/test_app/views/auth_views.py
@@ -23,37 +23,6 @@ def load_user(id):
         pass
 
 
-# @login_manager.request_loader
-# def load_user_from_request(request):
-
-#     # first, try to login using the api_key url arg
-#     api_key = request.args.get('api_key')
-#     if api_key:
-#         user = User.objects.get(api_key=api_key)
-#         if user:
-#             return user
-
-#     # next, try to login using Basic Auth
-#     auth_string = request.headers.get('Authorization')
-#     if auth_string:
-#         auth_string = auth_string.replace('Basic ', '', 1)
-#         try:
-#             auth_string = auth_string   # base64.b64decode(auth_string)
-#         except TypeError:
-#             pass
-#         username, password = auth_string.split(':')
-#         if (
-#               username == app.config['USER']['username']
-#                   and password == app.config['USER']['password']
-        # ):
-#             user = User.objects.get(username=username)
-#         if user:
-#             return user
-
-#     # finally, return None if both methods did not login the user
-#     return None
-
-
 class LoginView(BaseView):
     def post(self):
         data = {}
