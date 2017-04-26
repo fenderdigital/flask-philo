b4d7f2917ea6dcdc0893e30e12b589e70a3f9c15
Manuel Ignacio Franco Galeano <maigfrga@gmail.com>
2017-04-26 15:42:44 +0100

    new version of psqlutils, add update method to serializers

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='flaskutils',
-    version='1.0.8',
+    version='1.0.10',
     description='Flask Utilities',
     packages=['flaskutils', 'flaskutils.commands_flaskutils'],
     author='Manuel Ignacio Franco Galeano',
     author_email='maigfrga@gmail.com',
-    dependency_links=['https://github.com/Riffstation/sqlalchemypostgresutils.git@1.0.4#egg=sqlalchemypostgresutils'],  # noqa
+    dependency_links=['https://github.com/Riffstation/sqlalchemypostgresutils.git@1.0.5#egg=sqlalchemypostgresutils'],  # noqa
     install_requires=[
         'jsonschema',
         'pytest',
