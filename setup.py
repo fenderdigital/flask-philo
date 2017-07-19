90d42e675085b5804912b0dda4589d8b2d98b680
Manuel Ignacio Franco Galeano <maigfrga@gmail.com>
2017-07-19 12:25:03 +0100

    flake8

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name='flaskutils',
     version='2.0.0',
     description='Flask Utilities',
-    packages=['flaskutils', 'flaskutils.commands_flaskutils', 'flaskutils.db', 'flaskutils.db.postgresql'],
+    packages=[
+        'flaskutils', 'flaskutils.commands_flaskutils', 'flaskutils.db',
+        'flaskutils.db.postgresql'],
     author='Manuel Ignacio Franco Galeano',
     author_email='maigfrga@gmail.com',
     install_requires=[
