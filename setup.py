082517c682c658bc1e77b06e0b6d655e12444f6b
Manuel Ignacio Franco Galeano <maigfrga@gmail.com>
2017-08-02 17:04:18 +0100

    redis conector

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='flaskutils',
-    version='2.1.0',
+    version='2.2.0',
     description='Flask Utilities',
     packages=[
         'flaskutils', 'flaskutils.commands_flaskutils', 'flaskutils.db',
