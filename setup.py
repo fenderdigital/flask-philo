71998929ae1b1103f26a83a91663056386e4d015
vcali <vitorcali94@gmail.com>
2018-07-31 11:22:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Philo',
-    version='3.5.0',
+    version='3.5.1',
     description='Simple web framework based on Flask',
     long_description='Flask based framework to build RESTful microservices',
     packages=[
